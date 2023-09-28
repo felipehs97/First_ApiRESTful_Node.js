7292cb9db50929f6ec0b2619816c2ff175d2450c
Felipe Araujo <felipemraraujo@gmail.com>
2023-09-28 10:18:16 -0300

    add users/header

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
 import http from 'node:http'
 
 // Criando primeiro servidor Http
+
+const users = []
+
 const server = http.createServer ((request, response)=>{
-    return response.end("Hello ")
+    const {method, url} = request
+
+    if (method === "GET" && url === "/users"){
+        // Early Return (quando bate no early return o que está abaixo não é executado)
+        return response
+        .setHeader("Content-type", "application/json")
+        .end(JSON.stringify(users))
+    }
+    if (method === "POST" && url === "/users"){
+        users.push({
+            id:1,
+            name: "John Doe",
+            email: "johndoe@example.com",
+        })
+        return response.end("Criação de usuários")
+
+    }
+
+    return response.end("Hello World")
 })
 
 server.listen(3333);
