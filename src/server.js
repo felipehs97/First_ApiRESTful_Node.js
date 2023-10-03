aca71ae44da631a17d7199b396cc14ac064f8ddd
Felipe Araujo <felipemraraujo@gmail.com>
2023-10-03 09:41:52 -0300

    requisition body

--- src/server.js
+++ src/server.js
@@ -4,8 +4,20 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer ((request, response)=>{
+const server = http.createServer (async(request, response)=>{
     const {method, url} = request
+
+    const buffers = []
+
+   for await (const chunk of request){
+    buffers.push(chunk)
+   }
+
+   try{
+   request.body = JSON.parse(Buffer.concat(buffers).toString())
+   }catch{
+    request.body = null
+   }
 
     if (method === "GET" && url === "/users"){
         // Early Return (quando bate no early return o que está abaixo não é executado)
@@ -14,16 +26,18 @@ const server = http.createServer ((request, response)=>{
         .end(JSON.stringify(users))
     }
     if (method === "POST" && url === "/users"){
+        const {name, email} = request.body
+
         users.push({
             id:1,
-            name: "John Doe",
-            email: "johndoe@example.com",
+            name,
+            email,
         })
-        return response.end("Criação de usuários")
 
+        return response.writeHead(201).end()
     }
 
-    return response.end("Hello World")
+    return response.writeContinue(404).end()
 })
 
 server.listen(3333);
