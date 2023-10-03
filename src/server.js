8d3ce56df59ea2d07c883f624f2c65e036c35699
Felipe Araujo <felipemraraujo@gmail.com>
2023-10-03 10:01:12 -0300

    add middlewares

--- src/server.js
+++ src/server.js
@@ -1,28 +1,15 @@
 import http from 'node:http'
-
-// Criando primeiro servidor Http
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer (async(request, response)=>{
     const {method, url} = request
 
-    const buffers = []
-
-   for await (const chunk of request){
-    buffers.push(chunk)
-   }
-
-   try{
-   request.body = JSON.parse(Buffer.concat(buffers).toString())
-   }catch{
-    request.body = null
-   }
+    await json (request, response)
 
     if (method === "GET" && url === "/users"){
-        // Early Return (quando bate no early return o que está abaixo não é executado)
         return response
-        .setHeader("Content-type", "application/json")
         .end(JSON.stringify(users))
     }
     if (method === "POST" && url === "/users"){
